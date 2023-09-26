a2873a0fcc8b02503e6d1dd801c3178c2ac7a1ee
monzuralam <monzur707@gmail.com>
2023-09-26 15:31:22 +0600

    assets path has been updated

--- functions.php
+++ functions.php
@@ -5,8 +5,8 @@
  * @return void
  */
 function montana_assets(){
-    wp_enqueue_style( 'vue-style', get_template_directory_uri() . '/assets/styles.css', array(), time(), 'all' );
+    wp_enqueue_style( 'vue-style', get_template_directory_uri() . '/assets/css/styles.css', array(), time(), 'all' );
 
-    wp_enqueue_script( 'vue-bundle', get_template_directory_uri() . '/assets/bundle.js', array('jquery'), time(), true );
+    wp_enqueue_script( 'vue-bundle', get_template_directory_uri() . '/assets/js/bundle.js', array('jquery'), time(), true );
 }
 add_action( 'wp_enqueue_scripts', 'montana_assets' );
