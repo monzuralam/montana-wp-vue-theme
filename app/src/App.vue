3932df819d232830660329a4551f2d8059007bbf
monzuralam <monzur707@gmail.com>
2023-09-26 16:23:43 +0600

    Footer component has been added

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from "./components/Header/Header.Component.vue";
+import Footer from "./components/Footer/Footer.Component.vue";
 </script>
 
 <template>
   <Header />
+  <Footer />
 </template>
 
 <style scoped>
