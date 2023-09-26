3216cefbac592b42d7f47ed4d5d6c410e72abbeb
monzuralam <monzur707@gmail.com>
2023-09-26 16:40:23 +0600

    Home route component has been created

---
+++ app/src/routes/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <section>
+        <div class="container mx-auto">
+            <h1 class="text-3xl">Home</h1>
+        </div>
+    </section>
+</template>
+<script>
+export default{
+    name: 'Home'
+}
+</script>
